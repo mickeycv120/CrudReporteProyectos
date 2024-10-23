57b65131a6b4752f08d3343ab5deebf15d6ffce9
Efrain Coutiño <l22141208@queretaro.tecnm.mx>
2024-10-23 12:17:55 -0600

    Creation of Roles

--- src/main/java/com/mickey/web/app/crudspring/models/entity/Roles.java
+++ src/main/java/com/mickey/web/app/crudspring/models/entity/Roles.java
@@ -1,4 +1,30 @@
 package com.mickey.web.app.crudspring.models.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
 public class Roles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_rol")
+    private Long idRol;
+
+    private String rol;
+
+    public void setIdRol(Long idRol) {
+        this.idRol = idRol;
+    }
+
+    public Long getIdRol() {
+        return idRol;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
 }
